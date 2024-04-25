8f2d37882552867aecdaa30458800450790e4491
Diego Tejada <stanleytejadaj@gmail.com>
2024-04-24 23:13:43 -0700

    Create new user and login to the account

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-db = SQLAlchemy(
-    
-)
+db = SQLAlchemy()
 
 DB_NAME = 'database.db'
 
@@ -16,9 +15,16 @@ def create_app():
     
     from .views import views
     from .auth import auth
-
+    
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
     
     return app
+
+
+
--- website/auth.py
+++ website/auth.py
@@ -1,32 +1,60 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email='email').first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+                
+            else:
+                flash('Incorrect password!', category='error')
+        else:
+            flash('Username doesn\'t exist!', category='error')
     
     return render_template('login.html', boolean=True)
 
 
-
+#is a decorator used to know the path and method
 @auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
     
     if request.method == 'POST':
+        #get the data sendding from forms of sign-up
         email = request.form.get('email')
         password = request.form.get("password")
         password2 = request.form.get('password2')
         
-        if len(email) < 8:
+        
+        user = User.query.filter_by(email='email').first()
+        
+        #show massage of error or success in order to everything is correct
+        if user:
+            flash('User already exists!', category='error')
+        elif len(email) < 8:
             flash('Email must be greater than 8 characters', category='error')
         elif password != password2:
             flash('Password don\'t match', category='error')
         elif len(password) < 8:
             flash('Password must be greater than 8 character', category='error')
         else:
+            #create new user and send data to our database
+            new_user = User(email = 'email', password = generate_password_hash(password, method='pbkdf2:sha256')  )
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account has been created succesfully!', category='success')
-          
-        data=request.form  
-        print(data)
-    
+            
+            return redirect(url_for('views.home'))
+            
+    #return the template.html 
     return render_template('signup.html')
